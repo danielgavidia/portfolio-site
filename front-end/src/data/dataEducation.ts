7666fa451057673fc42ce161a59c7da9daabc761
Daniel Gavidia <dgavidia1@gmail.com>
2024-09-23 15:15:20 -0400

    Added initial professional/educational data

--- front-end/src/data/dataEducation.ts
+++ front-end/src/data/dataEducation.ts
@@ -3,12 +3,16 @@ export const dataEducation = [
         institution: "Duke University",
         dateStart: "August 2020",
         dateEnd: "May 2022",
-        description: ["Line 1", "Line 2", "Line 3"],
+        description: [
+            "Full-tuition merit scholarship recipient",
+            "Finance Club cabinet",
+            "Selected coursework: Data Analytics for Business, Valuation and Fundamental Analysis, Derivatives",
+        ],
     },
     {
         institution: "University of Southern California",
         dateStart: "August 2013",
         dateEnd: "May 2017",
-        description: ["Line 1", "Line 2", "Line 3"],
+        description: ["Full-tuition merit scholarship recipient."],
     },
 ];
