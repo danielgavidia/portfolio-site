53dc92e2b67804674d1e0ff8737d13a5e00272d1
Daniel Gavidia <dgavidia1@gmail.com>
2024-09-23 14:20:27 -0400

    Added simple project cards

--- front-end/src/components/Home.tsx
+++ front-end/src/components/Home.tsx
@@ -1,5 +1,70 @@
+import ProjectCard from "./ProjectCard";
+
+const data = [
+    {
+        id: 0,
+        title: "project1",
+        body: "this is project1!",
+        image: "",
+        linkSite: "",
+        linkGithub: "https://github.com/danielgavidia/portfolio-site",
+        linkDemo: "",
+    },
+    {
+        id: 1,
+        title: "project2",
+        body: "this is project2!",
+        image: "",
+        linkSite: "",
+        linkGithub: "",
+        linkDemo: "",
+    },
+    {
+        id: 2,
+        title: "project3",
+        body: "this is project3",
+        image: "",
+        linkSite: "",
+        linkGithub: "",
+        linkDemo: "",
+    },
+    {
+        id: 3,
+        title: "project4",
+        body: "this is project4!",
+        image: "",
+        linkSite: "",
+        linkGithub: "",
+        linkDemo: "",
+    },
+    {
+        id: 4,
+        title: "project5",
+        body: "this is project5!",
+        image: "",
+        linkSite: "",
+        linkGithub: "",
+        linkDemo: "",
+    },
+    {
+        id: 5,
+        title: "project6",
+        body: "this is project6",
+        image: "",
+        linkSite: "",
+        linkGithub: "",
+        linkDemo: "",
+    },
+];
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="grid grid-cols-3 gap-4">
+            {data.map((x) => {
+                return <ProjectCard key={x.id} project={x} />;
+            })}
+        </div>
+    );
 };
 
 export default Home;
