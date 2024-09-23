8880640d8dab51ecb77334bd8797b0ad464aa5a4
Daniel Gavidia <dgavidia1@gmail.com>
2024-09-23 13:04:40 -0400

    DaisyUI/Tailwind fix

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,8 @@
 const App = () => {
     return (
         <div className="">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+            <h1 className="">Hello world!</h1>
+            <button className="btn btn-primary text-3xl text-red-500">
                 Button
             </button>
         </div>
